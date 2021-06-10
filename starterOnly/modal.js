00f99a65b79b603e0609e523141532d5c263f378
maescristro <milfortchristian@gmail.com>
2021-06-10 08:53:32 +0200

    gitignore

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -68,11 +68,13 @@ function validate() {
   const conditions = document.querySelector('#checkbox1').checked;
   const nextevents = document.querySelector("#checkbox2");
 
+  const inputFirst = document.querySelector('#first');
+
   //regular expression for email
   const emailReg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
   //Conditions
-console.log(conditions);
+  console.log(conditions);
   //The First Name field has a minimum of 2 characters / is not empty.
   if(firstName !== '' && firstName.length >=2) {
     //the last name field has a minimum of 2 characters / is not empty.
@@ -90,6 +92,7 @@ console.log(conditions);
                 if(conditions == true) {
                   console.log('formulaire valide')
                   alert("Merci ! Votre réservation a été reçue.")
+                  inputFirst.addclass('form-valid');
                   return true;
                 } else {
                   alert("Vous devez vérifier que vous acceptez les termes et conditions.");
@@ -115,7 +118,9 @@ console.log(conditions);
       return false;
     }
   } else {
+    
     alert("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+    
     return false;
   } 
 
