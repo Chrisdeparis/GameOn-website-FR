89c986b4797032c113cfde270c01fdd314fe4553
maescristro <milfortchristian@gmail.com>
2021-06-10 13:04:54 +0200

    last name valid

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelectorAll('.close');
-const submitBtn = document.querySelectorAll('.btn-submit');
-
-
-
-
-
+const closeBtn = document.querySelectorAll(".close");
+const submitBtn = document.querySelectorAll(".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,50 +23,50 @@ function launchModal() {
 }
 
 //close modal event
-closeBtn.forEach((btn) => btn.addEventListener('click', closeModal));
-
-
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // close the modal form
 function closeModal() {
   // if click outside the modal-body
-  document.addEventListener("click", e => {
+  document.addEventListener("click", (e) => {
     if (e.target == document.querySelector(".bground")) {
       modalbg.style.display = "none";
     }
   });
   // if pressing esc key
-  document.addEventListener("keyup", e => {
+  document.addEventListener("keyup", (e) => {
     if (e.key == "Escape" && document.querySelector(".bground")) {
       modalbg.style.display = "none";
     }
   });
-  return modalbg.style.display = "none";
+  return (modalbg.style.display = "none");
 }
 
 //submit form
-submitBtn.forEach((btn) => btn.addEventListener('click', validate));
+submitBtn.forEach((btn) => btn.addEventListener("click", validate));
+
 
 // when form is submitted
 function validate() {
-  const firstName = document.querySelector('#first').value;
-  const lastName = document.querySelector('#last').value;
-  const email = document.querySelector('#email').value;
-  const birthdate = document.querySelector('#birthdate').value;
-  const numberParticipation = document.querySelector('#quantity').value;
-  const newYork = document.querySelector('#location1');
+  const firstName = document.querySelector("#first").value;
+  const lastName = document.querySelector("#last").value;
+  const email = document.querySelector("#email").value;
+  const birthdate = document.querySelector("#birthdate").value;
+  const numberParticipation = document.querySelector("#quantity").value;
+  const newYork = document.querySelector("#location1");
   const sanFrancisco = document.querySelector("#location2");
-  const seattle =  document.querySelector('#location3');
-  const chicago = document.querySelector('#location4');
-  const boston = document.querySelector('#location5');
-  const portland = document.querySelector('#location6');
-  const conditions = document.querySelector('#checkbox1').checked;
+  const seattle = document.querySelector("#location3");
+  const chicago = document.querySelector("#location4");
+  const boston = document.querySelector("#location5");
+  const portland = document.querySelector("#location6");
+  const conditions = document.querySelector("#checkbox1").checked;
   const nextevents = document.querySelector("#checkbox2");
 
-  const inputFirst = document.querySelector('#first');
-  const inputLast = document.querySelector('#last');
-  const invalidFirst = document.querySelector('.invalid-msg');
-  const check = document.querySelector('.valid-check');
+  const inputFirst = document.querySelector("#first");
+  const inputLast = document.querySelector("#last");
+  const invalidFirst = document.querySelector(".invalid-first");
+  const invalidLast = document.querySelector(".invalid-last");
+  const check = document.querySelector(".valid-check");
 
   //regular expression for email
   const emailReg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -79,63 +74,74 @@ function validate() {
   //Conditions
   console.log(conditions);
   //The First Name field has a minimum of 2 characters / is not empty.
-  if(firstName !== '' && firstName.length >=2) {
-    inputFirst.classList.remove('form-invalid');
-    inputFirst.classList.add('form-valid');
-    check.classList.add('show');
-
-    console.log(inputFirst);
-    invalidFirst.innerHTML = '';
+  if (firstName !== "" && firstName.length >= 2) {
+    inputFirst.classList.remove("form-invalid");
+    inputFirst.classList.add("form-valid");
+    check.classList.add("show-first");
+    invalidFirst.innerHTML = "";
     //the last name field has a minimum of 2 characters / is not empty.
-    if(lastName !== '' && lastName.length >=2) {
+    if (lastName !== "" && lastName.length >= 2) {
+      inputLast.classList.remove("form-invalid");
+      inputLast.classList.add("form-valid");
+      check.classList.add("show-last");
+      invalidLast.innerHTML = "";
       //The email address is valid.
-      if(email.match(emailReg)) {
+      if (email.match(emailReg)) {
         // birthdate ne doit pas etre vide
-        if(birthdate !== '') {
+        if (birthdate !== "") {
           //For the number of contests, a numerical value is entered.
-          if(numberParticipation !== '' || numberParticipation == 0) {
+          if (numberParticipation !== "" || numberParticipation == 0) {
             //A radio button is selected.
-            if( newYork.checked || sanFrancisco.checked || seattle.checked || chicago.checked || boston.checked || portland.checked  ) {
-                //The general conditions box is checked, the other box is optional / can be left unchecked.
-                console.log(conditions);
-                if(conditions == true) {
-                  console.log('formulaire valide')
-                  alert("Merci ! Votre réservation a été reçue.")
-                  
-                  return true;
-                } else {
-                  alert("Vous devez vérifier que vous acceptez les termes et conditions.");
-                  return false;
-                }
-                
+            if (
+              newYork.checked ||
+              sanFrancisco.checked ||
+              seattle.checked ||
+              chicago.checked ||
+              boston.checked ||
+              portland.checked
+            ) {
+              //The general conditions box is checked, the other box is optional / can be left unchecked.
+              console.log(conditions);
+              if (conditions == true) {
+                console.log("formulaire valide");
+                alert("Merci ! Votre réservation a été reçue.");
+
+                return true;
+              } else {
+                alert(
+                  "Vous devez vérifier que vous acceptez les termes et conditions."
+                );
+                return false;
+              }
             } else {
               alert("Vous devez choisir une option.");
               return false;
             }
-          } 
-
+          }
         } else {
-          alert("Vous devez entrer votre date de naissance.")
+          alert("Vous devez entrer votre date de naissance.");
           return false;
         }
       } else {
         alert("votre email n'est pas valide");
         return false;
       }
-    } else{
-      alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    } else {
+      inputLast.classList.remove("form-valid");
+      inputLast.classList.add("form-invalid");
+      check.classList.remove("show-last");
+      invalidLast.innerHTML =
+        "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+      // alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
       return false;
     }
   } else {
-    inputFirst.classList.remove('form-valid');
+    inputFirst.classList.remove("form-valid");
     inputFirst.classList.add("form-invalid");
-    check.classList.remove('show');
-    console.log(inputFirst);
-    invalidFirst.innerHTML ='Veuillez entrer 2 caractères ou plus pour le champ du prénom.';
-    // alert("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+    check.classList.remove("show-first");
+    invalidFirst.innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     
     return false;
-  } 
-
+  }
 }
-
